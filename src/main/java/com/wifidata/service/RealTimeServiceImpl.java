54030dd3003d040492687e32e73fe5421b56a359
MasamiYui <yingey@sina.com>
2017-05-31 20:51:55 +0800

    qqq

--- src/main/java/com/wifidata/service/RealTimeServiceImpl.java
+++ src/main/java/com/wifidata/service/RealTimeServiceImpl.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by yangchun on 2017/5/19.
@@ -33,8 +30,13 @@ public class RealTimeServiceImpl implements RealTimeService {
 
     public List<HashMap<String, Object>> getRealTimeCustomer() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.MINUTE,-1);
         List<HashMap<String, Object>> customerTimeInfoList = customerTimeDao.getCustomerTimeInfo();//在每日维护的用户表中查询实时的用户是否为新老客户
-        List<String> realTimeCustomerList = realTimeDao.getRealCustomer(simpleDateFormat.format(new Date()));
+        List<String> realTimeCustomerList = realTimeDao.getRealCustomer(simpleDateFormat.format(calendar.getTime()));
+
         HashMap<String, Object> map = null;
         List<HashMap<String, Object>> resultList = new ArrayList<HashMap<String, Object>>();
         boolean b = false;
@@ -44,7 +46,7 @@ public class RealTimeServiceImpl implements RealTimeService {
                     b = true;
                     map = new HashMap<String, Object>();
                     map.put("mac",realTimeCustomerList.get(i));
-                    map.put("time",customerTimeInfoList.get(j).get("lasttime"));
+                    map.put("time",simpleDateFormat2.format(new Date(Long.parseLong(customerTimeInfoList.get(j).get("lasttime").toString()))));
                     map.put("ctype","1");
                     resultList.add(map);
                 }
